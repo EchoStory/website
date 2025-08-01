78b5354c7b0aa3d35605ae3de64e322aa84e068a
Cartoon Kritthapth Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2025-08-01 14:02:17 +0700

    Change nav to tailwind docs

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -21,7 +21,7 @@
             d="M4 6h16M4 12h8m-8 6h16" />
         </svg>
         </div>
-        <ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li><a>Item 1</a></li>
         <li>
             <a>Parent</a>
