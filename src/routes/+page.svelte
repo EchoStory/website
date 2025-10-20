3c26e2ca186f10d3c3dbc8db8bf536a726c9dec6
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2025-10-21 04:41:56 +0700

    update: starting to put data onto site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,35 +4,82 @@
 
 <Hee />
 
-<div class="flex    justify-center">
-    <div class="carousel" style="max-width: 560px;">
-        <div id="slide1" class="carousel-item relative w-full">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/wLxJF91MPzU?si=Qr71sU1D6TYZoXgc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" class="btn btn-circle">❮</a>
-            <a href="#slide2" class="btn btn-circle">❯</a>
+<div class="flex flex-col justify-center items-center m-7">
+    <div class="container">
+        <div class="text-xl text-center mt-2.5">
+            <span>From 2016, EchoStory that founded as</span>
+            <span class="font-bold">Cartoon Production,</span>
+            <span>making content on Youtube, as the founder dream </span>
+            <span class="font-bold">of being content creator from that day.</span>
         </div>
+        <div class="text-xl text-center">
+            <span>Then became</span>
+            <span class="font-bold">StoryHouse,</span>
+            <span>and today with new content, creator</span>
+            <span class="font-bold">that ready to echo and spread story to the world!</span>
         </div>
-        <div id="slide2" class="carousel-item relative w-full">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/508EjhsN8yU?si=fAsx9vCauWF6Hwfc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide1" class="btn btn-circle">❮</a>
-            <a href="#slide3" class="btn btn-circle">❯</a>
+        <div class="flex container mt-7 justify-center items-center">
+            <div class="flex flex-col lg:flex-row gap-10">
+                <div class="items-center text-center">
+                    <div class="avatar">
+                        <div class="w-50 rounded-full">
+                            <img src="/logo/CartoonProductionLogo_1.png" alt="Cartoon Production Logo from 2019"/>
+                        </div>
+                    </div>
+                    <div class="text-center mt-4">
+                        <p class="text-4xl font-bold">2016 - 2022</p>
+                        <p class="text-2xl">Cartoon Production</p>
+                    </div>
+                </div>
+                <div class="items-center text-center">
+                    <div class="avatar">
+                        <div class="w-50 rounded-full">
+                            <img src="/logo/StoryHouseLogo.jpg" alt="StoryHouse Logo from 2022"/>
+                        </div>
+                    </div>
+                    <div class="text-center mt-4">
+                        <p class="text-4xl font-bold">2022 - 2025</p>
+                        <p class="text-2xl">StoryHouse</p>
+                    </div>
+                </div>
+                <div class="items-center text-center">
+                    <div class="avatar">
+                        <div class="w-50 rounded-full">
+                            <img src="/logo/EchoStoryLogo.jpg" alt="EchoStory Logo"/>
+                        </div>
+                    </div>
+                    <div class="text-center mt-4">
+                        <p class="text-4xl font-bold">Present</p>
+                        <p class="text-2xl">EchoStory</p>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+
+    <div class="container mt-20">
+        <div class="text-center">
+                <p class="text-5xl font-bold">"More Than Production"</p>
         </div>
-        <div id="slide3" class="carousel-item relative w-full">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/4vaRMaZ5x2g?si=FXVGaQWcbIdx49AS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide2" class="btn btn-circle">❮</a>
-            <a href="#slide4" class="btn btn-circle">❯</a>
+        <div class="flex flex-col lg:flex-row mt-10">
+            <div class="flex-1 text-center">
+                <p class="text-5xl font-bold">Vision</p>
+                <p class="text-2xl mt-5">Build the future of storytelling, merging creative production and technology.</p>
+            </div>
+            <div class="flex-1 text-center">
+                <p class="text-5xl font-bold">Mission</p>
+                <p class="text-2xl mt-5">Merge artistry with code; producing content onto our platforms (YouTube, Blog, Podcast) and developing digital products that tell their stories.</p>
+            </div>
         </div>
-        </div>
-        <div id="slide4" class="carousel-item relative w-full">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/3h8fla8LnAg?si=fIJio50PeEsQ8p8E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" class="btn btn-circle">❮</a>
-            <a href="#slide1" class="btn btn-circle">❯</a>
-        </div>
+    </div>
+    
+    <div class="container mt-20">
+        <div class="flex flex-col lg:flex-row gap-5">
+            <img class="lg:w-1/2 flex-1 rounded-lg shadow-xl/45" src="/assets/echostory_works.svg" alt="All of EchoStory Published.">
+            <div class="flex-1 content-center">
+                <h1 class="font-bold text-6xl">Meet our works!</h1>
+                <p class="mt-4">EchoStory has been working with a lot of project, content, style and creator on almost every type, like Short-Formed Video, Long-Formed Video, Podcast, Blog Writing, Website/Forums/Community Hosting. In every platforms like Youtube, Facebook, Instagram, Tiktok, Website, Github, etc.</p>
+            </div>
         </div>
     </div>
 </div>
